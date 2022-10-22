d182a61ea1f247e1986d4c8eac8775856b7bcb22
Vlad Bolea <53857958+vladbolea@users.noreply.github.com>
2022-10-22 19:11:34 +0300

    some ui fixes (#1)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,38 +1,50 @@
+import { motion } from 'framer-motion';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import Loader from '../static/icons/loader.svg';
 
 export const Navbar: FC = () => {
   const { data: session, status } = useSession();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [open]);
 
   return (
     <>
-      <div className="sticky top-0 z-50 flex h-20 w-full items-center justify-around bg-[#000000b6] backdrop-blur-xl">
-        <div className="hover:cursor-pointer">
+      <div className="sticky top-0 z-50 flex h-20 w-full items-center justify-between bg-[#000000b6] backdrop-blur-xl md:justify-around">
+        <div className="ml-5 hover:cursor-pointer md:ml-0">
           <Link href={'/'}>
             <p className="text-xl text-white">
               <strong>Play</strong>Cloud
             </p>
           </Link>
         </div>
-        <div className="">
+        <div className="hidden md:block">
           <p className="text-gray-400">
             Spotify&nbsp;&nbsp; {'>'} &nbsp;&nbsp;
             <span className="text-white">Popular playlists</span>
           </p>
         </div>
-        <div className="">
+        <div className="hidden md:block">
           <p className="text-gray-400">Playlist Migrator</p>
         </div>
-        <Link href="/playlists/me/spotify">
-          <p className="text-gray-400 transition-all hover:cursor-pointer hover:text-white">
-            Your Playlists
-          </p>
-        </Link>
-        <div className="">
+        <div className="hidden md:block">
+          <Link href="/playlists/me/spotify">
+            <p className="text-gray-400 transition-all hover:cursor-pointer hover:text-white">
+              Your Playlists
+            </p>
+          </Link>
+        </div>
+        <div className="mr-5 flex md:mr-0 md:block">
           {status === 'loading' ? (
             <div className="grid w-[86.94px] place-content-center">
               <Image
@@ -46,7 +58,7 @@ export const Navbar: FC = () => {
           ) : session ? (
             <Link href={'/profile'}>
               <div className="flex items-center hover:cursor-pointer">
-                <div className="relative aspect-square h-8 w-8 overflow-hidden rounded-md">
+                <div className="relative aspect-square h-8 overflow-hidden rounded-md">
                   <Image
                     src={session?.user?.image as string}
                     layout={'fill'}
@@ -60,14 +72,69 @@ export const Navbar: FC = () => {
               </div>
             </Link>
           ) : (
-            <Link href={'/api/auth/signin'}>
+            <Link href={'/signin'}>
               <div className="rounded-md border-[1px] border-white px-5 py-1">
                 <p className=" text-white hover:cursor-pointer">Login</p>
               </div>
             </Link>
           )}
+          {
+            //6px
+          }
+          <div
+            onClick={() => {
+              setOpen(!open);
+            }}
+            className="z-50 ml-5 aspect-square w-8 hover:cursor-pointer md:hidden"
+          >
+            <motion.div
+              animate={{
+                y: open ? '4px' : 0,
+                rotateZ: open ? '-45deg' : 0,
+              }}
+              transition={{
+                ease: 'backInOut',
+                duration: 0.3,
+              }}
+              className=" mb-[6px] mt-[10px] h-[3px] w-[32px] rounded-lg bg-gray-400"
+            ></motion.div>
+            <motion.div
+              animate={{
+                y: open ? '-5px' : 0,
+                rotateZ: open ? '45deg' : 0,
+              }}
+              transition={{
+                ease: 'backInOut',
+                duration: 0.3,
+              }}
+              className=" h-[3px] w-[32px] rounded-lg bg-gray-400"
+            ></motion.div>
+          </div>
         </div>
       </div>
+      <SideBar open={open} />
+    </>
+  );
+};
+
+const SideBar = ({ open }: { open: boolean }) => {
+  return (
+    <>
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: '100vw',
+        }}
+        animate={{
+          x: open ? 0 : '100vw',
+          opacity: open ? [0, 0.2, 1] : 0,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: 'easeInOut',
+        }}
+        className="fixed top-0 left-0 z-30 h-screen w-screen bg-[#282828]"
+      ></motion.div>
     </>
   );
 };
--- src/pages/api/provider/handler.ts
+++ src/pages/api/provider/handler.ts
@@ -1,77 +1,92 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerAuthSession } from '../../../server/common/get-server-auth-session';
 import { prisma } from '../../../server/db/client';
 import { refreshAccessToken } from '../auth/[...nextauth]';
 
 const providerHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.method);
+  const session = await getServerAuthSession({ req, res });
+  console.log('Post request');
+  console.log(`session ${JSON.stringify(session)}`);
 
   switch (req.method) {
     case 'POST':
       const { currentProvider } = req.body;
       const { neededProvider } = req.body;
-      const { email } = req.body;
+      const email = session?.user?.email as string;
 
       if (currentProvider === 'spotify') {
-        const userData = await prisma.user.findUnique({
-          where: {
-            email: email,
-          },
-          include: {
-            accounts: {
-              where: {
-                provider: neededProvider,
+        try {
+          const userData = await prisma.user.findUnique({
+            where: {
+              email: email,
+              id: session?.user?.id,
+            },
+            include: {
+              accounts: {
+                where: {
+                  provider: neededProvider,
+                },
               },
             },
-          },
-        });
-        res.status(200).json(userData);
-        return userData;
+          });
+          res.status(200).json(userData);
+          console.log(userData);
+        } catch (err) {
+          console.log(err);
+        }
       } else if (currentProvider === 'google') {
-        const userData = await prisma.user.findUnique({
-          where: {
-            email: email,
-          },
-          include: {
-            accounts: {
-              where: {
-                provider: neededProvider,
+        try {
+          const userData = await prisma.user.findUnique({
+            where: {
+              email: email,
+              id: session?.user?.id,
+            },
+            include: {
+              accounts: {
+                where: {
+                  provider: neededProvider,
+                },
               },
             },
-          },
-        });
-
-        const accessToken = userData?.accounts[0]?.access_token;
-        const refreshToken = userData?.accounts[0]?.refresh_token;
-        const expiresAt = userData?.accounts[0]?.expires_at;
-
-        if (
-          expiresAt &&
-          expiresAt * 1000 < Date.now() &&
-          accessToken &&
-          refreshToken
-        ) {
-          const newToken = await refreshAccessToken({
-            accessToken,
-            refreshToken,
-            provider: 'spotify',
-            accessTokenExpires: expiresAt,
           });
 
-          if (userData.accounts[0]?.access_token) {
-            userData.accounts[0].access_token = newToken?.accessToken as string;
-
-            await prisma.account.update({
-              where: {
-                id: userData.accounts[0].id,
-              },
-              data: {
-                access_token: newToken?.accessToken as string,
-              },
+          const accessToken = userData?.accounts[0]?.access_token;
+          const refreshToken = userData?.accounts[0]?.refresh_token;
+          const expiresAt = userData?.accounts[0]?.expires_at;
+          if (
+            expiresAt &&
+            expiresAt * 1000 < Date.now() &&
+            accessToken &&
+            refreshToken
+          ) {
+            const newToken = await refreshAccessToken({
+              accessToken,
+              refreshToken,
+              provider: 'spotify',
+              accessTokenExpires: expiresAt,
             });
-          }
-        }
 
-        res.status(200).json(userData);
+            if (userData.accounts[0]?.access_token) {
+              userData.accounts[0].access_token =
+                newToken?.accessToken as string;
+
+              await prisma.account.update({
+                where: {
+                  id: userData.accounts[0].id,
+                },
+                data: {
+                  access_token: newToken?.accessToken as string,
+                },
+              });
+            }
+          }
+
+          console.log(userData);
+
+          res.status(200).json(userData);
+        } catch (err) {
+          console.log(err);
+        }
       }
   }
 };

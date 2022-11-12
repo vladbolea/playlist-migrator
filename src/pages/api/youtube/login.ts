e087e0cbe39f36f499246f370dd50778c59f5a75
Vlad <vladbolea04@gmail.com>
2022-11-12 14:50:47 +0200

    added search api for each track

--- src/pages/api/youtube/login.ts
+++ src/pages/api/youtube/login.ts
@@ -4,6 +4,10 @@ import YoutubeSearchApiResponse from '../../../interfaces/youtube-search.js';
 import { getServerAuthSession } from '../../../server/common/get-server-auth-session';
 
 const playlists = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerAuthSession({ req, res });
+  const googleAccessToken = req.body.googleAccessToken;
+
+  
 
   res.status(200).json({ data: { somedata: 'merge' } });
 };
